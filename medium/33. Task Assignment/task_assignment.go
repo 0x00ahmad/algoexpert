e3c2fb04b85a7cc8a391494606353606870ec48a
0xaf2f6ad <a.26417357@pm.me>
2022-09-26 17:06:59 +0500

    Medium: 33

--- medium/33. Task Assignment/task_assignment.go
+++ medium/33. Task Assignment/task_assignment.go
@@ -1,6 +1,30 @@
 package main
 
+import "sort"
+
+type Pair struct {
+	val int
+	idx int
+}
+
+// O(n log n) time | O(n) space
 func TaskAssignment(k int, tasks []int) [][]int {
-	// Write your code here.
-	return nil
+	if k == 1 {
+		return [][]int{{0, 1}}
+	}
+	assignments := make([][]int, 0)
+	sorted_tasks := make([]Pair, 0)
+	for i, e := range tasks {
+		sorted_tasks = append(sorted_tasks, Pair{e, i})
+	}
+	sort.Slice(sorted_tasks, func(a, b int) bool {
+		return sorted_tasks[a].val < sorted_tasks[b].val
+	})
+	lower, upper := 0, len(tasks)-1
+	for lower < upper {
+		assignments = append(assignments, []int{sorted_tasks[lower].idx, sorted_tasks[upper].idx})
+		upper--
+		lower++
+	}
+	return assignments
 }
