97ee839b9da5138bc2977ae6f6b35cb25f23f6ae
0xaf2f6ad <a.26417357@pm.me>
2022-11-23 04:20:36 +0500

    medium: 43 - a more preferred solution

--- medium/43. Search In Sorted Matrix/search_in_sorted_matrix.py
+++ medium/43. Search In Sorted Matrix/search_in_sorted_matrix.py
@@ -10,3 +10,16 @@ def searchInSortedMatrix(matrix, target: int):
             except ValueError:
                 continue
     return out
+
+
+def searchInSortedMatrix_with_row_col_pointers(matrix, target: int):
+    row = 0
+    col = len(matrix[0]) - 1
+    while row < len(matrix) and col >= 0:
+        if matrix[row][col] > target:
+            col -= 1
+        elif matrix[row][col] < target:
+            row += 1
+        else:
+            return [row, col]
+    return [-1, -1]
