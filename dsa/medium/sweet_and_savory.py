a58b0af5caefe1aa4a422b4cca04f22c4b2827ad
0x00ahmad <mahmadrehan00@gmail.com>
2024-03-04 11:00:58 +0200

    medium another one

--- dsa/medium/sweet_and_savory.py
+++ dsa/medium/sweet_and_savory.py
@@ -1,2 +1,23 @@
-def sweetAndSavory(dishes, target):
-    return []
+from typing import List
+
+
+def sweetAndSavory(dishes: List[int], target: int) -> List[int]:
+    sweet_dishes = sorted([dish for dish in dishes if dish < 0], key=abs)
+    savory_dishes = sorted([dish for dish in dishes if dish > 0], key=abs)
+
+    result = [0, 0]
+    closest_diff = float("inf")
+    sweet_idx, savory_idx = 0, 0
+    while sweet_idx < len(sweet_dishes) and savory_idx < len(savory_dishes):
+        current_sum = sweet_dishes[sweet_idx] + savory_dishes[savory_idx]
+
+        if current_sum <= target:
+            current_diff = target - current_sum
+            if current_diff < closest_diff:
+                closest_diff = current_diff
+                result = [sweet_dishes[sweet_idx], savory_dishes[savory_idx]]
+            savory_idx += 1
+            continue
+        sweet_idx += 1
+
+    return result
